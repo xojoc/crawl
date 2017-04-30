c11646a42c7b7d98c3d646e022cc7857fd6ee7d8
xojoc <hi@xojoc.pw>
2017-04-30 13:46:11 +0200

    s

---
+++ robots/README.md
@@ -0,0 +1,33 @@
+# robots.txt
+*robots* is a library written in [golang](https://golang.org) for parsing the robots.txt file format.
+
+# Usage
+First install the library with
+```
+go get xojoc.pw/crawl/robots
+```
+
+...then run...
+
+````
+go test
+``````
+
+if something fails open an issue.
+
+See [godoc](https://godoc.org/xojoc.pw/crawl/robots) for the complete documentation.
+
+# Why?
+
+ * Great and stable API
+ * Lots of tests and [quicktesting](https://golang.org/pkg/testing/quick/)!
+ * [Good documentation](https://godoc.org/xojoc.pw/robots/crawl) with lots of examples.
+
+# Who?
+*robots* was written by Alexandru Cojocaru (https://xojoc.pw).
+
+# [Donate!](https://liberapay.com/xojoc)
+
+# License
+*robots* is [Free Software](https://www.gnu.org/philosophy/free-sw.html) and under the Apache 2.0 license. No warranty.
+
---
+++ robots/robots.go
@@ -0,0 +1,116 @@
+// Written by https://xojoc.pw. Apache 2.0 license. No warranty.
+
+// Package robots parses a robots.txt file as specified by Wikipedia https://en.wikipedia.org/wiki/Robots.txt
+package robots // import "xojoc.pw/crawl/robots"
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"strconv"
+	"strings"
+)
+
+// Txt contains the robots.txt rules.
+type Txt struct {
+	CrawlDelay map[string]int
+	Allow      map[string][]string
+	Disallow   map[string][]string
+
+	Sitemaps []string
+}
+
+// Allowed returns true if ua can access path.
+// False otherwise.
+func (t *Txt) Allowed(ua string, path string) bool {
+	allowed := true
+
+	disallowedPath := ""
+
+	for _, d := range append(t.Disallow["*"], t.Disallow[ua]...) {
+		if d == "" {
+			allowed = true
+			disallowedPath = ""
+			continue
+		}
+		if strings.HasPrefix(path, d) {
+			if len(d) < len(disallowedPath) || disallowedPath == "" {
+				disallowedPath = d
+			}
+			allowed = false
+		}
+
+	}
+	for _, a := range append(t.Allow["*"], t.Allow[ua]...) {
+		if strings.HasPrefix(path, a) {
+			if len(a) > len(disallowedPath) {
+				allowed = true
+			}
+		}
+	}
+
+	return allowed
+}
+
+// Delay returns the number of seconds between successive accesses to
+// the same host.
+// Returns 0 if no delay is specified.
+func (t *Txt) Delay(ua string) int {
+	if i, ok := t.CrawlDelay[ua]; ok {
+		return i
+	}
+	return t.CrawlDelay["*"]
+}
+
+// Parse parses a robots.txt file.
+func Parse(r io.Reader) (*Txt, error) {
+	txt := &Txt{}
+	txt.CrawlDelay = map[string]int{}
+	txt.Disallow = map[string][]string{}
+	txt.Allow = map[string][]string{}
+
+	buf := bufio.NewReader(r)
+	var ua string
+
+	lua := []byte("User-agent: ")
+	ldis := []byte("Disallow:")
+	lall := []byte("Allow: ")
+	lsm := []byte("Sitemap: ")
+	lcd := []byte("Crawl-delay: ")
+
+	for {
+		l, err := buf.ReadSlice('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		l = l[:len(l)-1]
+
+		switch {
+		case bytes.HasPrefix(l, lua):
+			ua = string(l[len(lua):])
+		case bytes.HasPrefix(l, ldis):
+			dis := ""
+			if len(l) > len(ldis) {
+				dis = string(l[len(ldis)+1:])
+			}
+			txt.Disallow[ua] = append(txt.Disallow[ua], dis)
+		case bytes.HasPrefix(l, lall):
+			txt.Allow[ua] = append(txt.Allow[ua], string(l[len(lall):]))
+		case bytes.HasPrefix(l, lcd):
+			i, err := strconv.Atoi(string(l[len(lcd):]))
+			if err == nil {
+				txt.CrawlDelay[ua] = i
+			}
+		case bytes.HasPrefix(l, lsm):
+			txt.Sitemaps = append(txt.Sitemaps, string(l[len(lsm):]))
+		default:
+			// skip line
+		}
+	}
+
+	return txt, nil
+}
